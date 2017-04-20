63b435b1f353f429da0859169981cb6e4ccbbc69
kenellorando <kenellorando@gmail.com>
2017-04-19 19:19:20 -0500

    Created bare table MYSQL version

---
+++ mysql_ver.sql
@@ -0,0 +1,20 @@
+/* Creates and uses the database 'shop' */
+SHOW databases;
+CREATE DATABASE shop;
+USE shop;
+
+CREATE TABLE customers (
+	test VARCHAR(10)
+);
+
+CREATE TABLE employees (
+	test VARCHAR(10)
+);
+
+CREATE TABLE services (
+	test VARCHAR(10)
+);
+
+CREATE TABLE appointments (
+	test VARCHAR(10)
+);
